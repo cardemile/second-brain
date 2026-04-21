e7bdd87b3feffc2f2ddaf25169c8ad92143ac86e
Frajna <puspitafrajna@gmail.com>
2026-04-21 22:07:26 +0800

    fix: simplify service worker

--- sw.js
+++ sw.js
@@ -1,5 +1,7 @@
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', () => clients.claim());
-self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(clients.claim());
 });
